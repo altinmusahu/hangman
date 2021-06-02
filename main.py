fb40393636791e1164a1de643eb37f7d16ed424c
altinmusahu <altinmusahu6@gmail.com>
2021-06-02 18:59:19 +0200

    Added score extra features

--- main.py
+++ main.py
@@ -1,121 +1,122 @@
 import random
 
 print("Welcome to Hangman game!")
+print("You have 10 tries to guess word")
+
+
 fin = open('words.txt')
 words = [line.strip().lower() for line in fin.readlines()]
+figures = [
+    """
+    ---------
+    |       |
+    |       O
+    |       
+    |
+    |
+    |
+    """
+    ,
+    """
+    ---------
+    |       |
+    |       O
+    |       |
+    |
+    |
+    |
+    """
+    ,
+
+    """
+    ---------
+    |       |
+    |       O
+    |      / \ 
+    |       
+    |
+    |
+    """
+    ,
+
+    """
+    ---------
+    |       |
+    |       O
+    |      / \ 
+    |       |  
+    |
+    |
+    """
+    ,
+
+    """
+    ---------
+    |       |
+    |       O
+    |      / \ 
+    |       |
+    |      / \ 
+    |
+    """
+]
 
 while True:
     word = random.choice(words)
     length = len(word)
     print(f"Guess the word with {length} letters!")
+
+
+
+    word_list_easy = []
+    word_list_medium = []
+    word_list_hard = []
     word_list = []
+
+
+
     indx = 0
     for i in range(length):
         word_list.append('_')
     print(*word_list)
 
+    score_all = 0
+    score_one_game = 0
+
+    count = 0
+    fails = 0
     while True:
+        count = count + 1
+
         letter = input("Guess a letter: ")
         for i in range(length):
             if letter == word[i]:
                 word_list[i] = letter
 
-        if '_' not in word_list:
-            print("You won!")
+        if letter in word_list:
+            score_one_game += 5
+            score_all += 5
+            print('score :',score_one_game)
 
         if letter not in word_list:
-            print("---------")
-            print("|       |")
-            print("|       O")
-            print("|")
-            print("|")
-            print("|")
-            print("|")
+            fails += 1
+            score_one_game -= 5
+            score_all -= 5
+            print(figures[fails-1])
+            print('score :', score_one_game)
+
+
+        if fails >= len(figures):
+            print("You Failed!")
+            print("Your score: ", score_all)
+            input("Press enter to play again!")
+
+
+        if '_' not in word_list:
+            print("You won!")
+            print("Your score: ",score_all)
+
         print(*word_list)
-
-        while True:
-            letter = input("Guess a letter: ")
-            for i in range(length):
-                if letter == word[i]:
-                    word_list[i] = letter
-
-                if '_' not in word_list:
-                    print("You won!")
-
-                if letter not in word_list:
-                    print("---------")
-                    print("|       |")
-                    print("|       O")
-                    print("|       |")
-                    print("|")
-                    print("|")
-                    print("|")
-                print(*word_list)
-
-                while True:
-                    letter = input("Guess a letter: ")
-                    for i in range(length):
-                        if letter == word[i]:
-                            word_list[i] = letter
-
-                        if '_' not in word_list:
-                            print("You won!")
-
-                        if letter not in word_list:
-                            print("---------")
-                            print("|       |")
-                            print("|       O")
-                            print("|      / \ ")
-                            print("|")
-                            print("|")
-                            print("|")
-                        print(*word_list)
-
-                        while True:
-                            letter = input("Guess a letter: ")
-                            for i in range(length):
-                                if letter == word[i]:
-                                    word_list[i] = letter
-
-                            if '_' not in word_list:
-                                print("You won!")
-
-                            if letter not in word_list:
-                                print("---------")
-                                print("|       |")
-                                print("|       O")
-                                print("|      / \ ")
-                                print("|       |")
-                                print("|")
-                                print("|")
-                            print(*word_list)
-
-                            while True:
-                                letter = input("Guess a letter: ")
-                                for i in range(length):
-                                    if letter == word[i]:
-                                        word_list[i] = letter
-
-                                    if '_' not in word_list:
-                                        print("You won!")
-
-                                    if letter not in word_list:
-                                        print("---------")
-                                        print("|       |")
-                                        print("|       O")
-                                        print("|      / \ ")
-                                        print("|       |")
-                                        print("|      / \ ")
-                                        print("|")
-
-                                        print("You Failed")
-                                        play_again = input("Press enter to play again")
-                                    print(*word_list)
-                                    break
-
-
-
-
 
 
 
